e6d7b6709378283f8d6d1f71cd6d9314c3845ba3
sponee <kyle.sponheim@gmail.com>
2019-10-25 12:13:51 -0700

    use the OutboundTextProcessor

--- app/controllers/outbound_controller.rb
+++ app/controllers/outbound_controller.rb
@@ -2,6 +2,23 @@ class OutboundController < ApplicationController
   protect_from_forgery with: :null_session
 
   def create
-    render status: 200
+    render_failure and return unless setup
+    ::OutboundTextProcessor.run(@message, @to_number) ? render_success : render_failure
+  end
+
+  private
+
+  def setup
+    @message = params["message"]
+    @to_number = params["to_number"]
+    @message.present? && @to_number.present?
+  end
+
+  def render_success
+    render json: { message: "Your message is being processed"}, status: 200
+  end
+
+  def render_failure
+    render json: { message: "Your message could not be processed" }, status: 500
   end
 end
