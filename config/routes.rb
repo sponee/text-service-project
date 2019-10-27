f67bcc48509753272dfe1462aea28f3cbb2585b3
sponee <kyle.sponheim@gmail.com>
2019-10-27 16:00:11 -0700

    fix routing issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/', as: 'rails_admin'
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  root to: redirect('/admin')
   post '/outbound', to: 'outbound#create'
   post '/inbound', to: 'inbound#create'
 end
